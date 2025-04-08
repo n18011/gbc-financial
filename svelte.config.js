a1a19695c0a091fbe9e402ab43cb6c1b5942e23d
n18011 <n18011@std.it-college.ac.jp>
2025-04-09 00:33:08 +0900

    prerendering中のエラーを処理し、ビルドが中断しないように変更

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ status, path, referrer, referenceType }) => {
+        if (status === 404) {
+          console.warn(`404 error on path "${path}" from "${referrer}" (${referenceType})`);
+          // ビルドを中断しないようにするため、フォールバックのレスポンスを返すか、空の文字列を返します
+          return { html: '<p>ページが見つかりません</p>', status: 200 };
+        }
+      }
+    }
+  },
 	extensions: ['.svelte', '.svx']
 };
 
